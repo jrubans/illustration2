40c779b5b3f4a1d1bcfe8246d9c8a71354e33f38
Jānis Rubans <janis.rubans@cube.lv>
2022-05-08 22:28:27 +0300

    Adjust animation timings, add text from design spec

--- dist/index.php
+++ dist/index.php
@@ -13,16 +13,16 @@
 <div class="illustration-container">
 <div class="illustration">
     <div class="text-container text-1"><div><span>Raw json-rpc</span></div></div>
-    <div class="text-container text-2"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-3"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-4"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-5"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-6"><div><span>Lorem ipsum</span></div></div>
+    <div class="text-container text-2"><div><span>Block details</span></div></div>
+    <div class="text-container text-3"><div><span>Blockchain events</span></div></div>
+    <div class="text-container text-4"><div><span>Assets</span></div></div>
+    <div class="text-container text-5"><div><span>Transaction details</span></div></div>
+    <div class="text-container text-6"><div><span>Tokens</span></div></div>
     <div class="text-container text-7"><div><span>Create transactions</span></div></div>
-    <div class="text-container text-8"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-9"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-10"><div><span>Lorem ipsum</span></div></div>
-    <div class="text-container text-11"><div><span>Lorem ipsum</span></div></div>
+    <div class="text-container text-8"><div><span>Wallet details</span></div></div>
+    <div class="text-container text-9"><div><span>Address details</span></div></div>
+    <div class="text-container text-10"><div><span>Fees</span></div></div>
+    <div class="text-container text-11"><div><span>Transactions</span></div></div>
 
     <?php include('assets/svg/illustration2.svg'); ?>
 </div>
